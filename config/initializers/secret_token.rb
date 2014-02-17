e78966f05a2e3867485417366aa46c17100b1350
Michael Raimi <mar2260@columbia.edu>
2014-02-16 23:43:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterForZombies::Application.config.secret_key_base = '2ad36e025969bea822b379c5171453b3368298dab626135e329a1e8417c6d03ec78e8c3e966a4e69f8080aec00cc8b656ccecdf17a94b9e86d21fd518bc1b175'
